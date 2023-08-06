9706f73cb3df15b59454ffbfb8f78a2929062006
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-06 17:37:03 -0400

    Polygons added

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,11 +1,18 @@
-import { MapContainer, Marker, Popup, TileLayer, useMap} from "react-leaflet";
+import { MapContainer, Marker, Polygon, Popup, TileLayer, Tooltip, useMap} from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import "./Map.scss";
 import { useEffect, useState } from "react";
+import { LatLngExpression } from "leaflet";
 
 interface Coordinates {
   lat: number;
   lng: number;
+}
+
+interface PolygonData {
+  coordinates: LatLngExpression[]; // Use LatLngExpression for polygon coordinates
+  title: string;
+  color: string;
 }
 
 type Props = {
@@ -13,6 +20,34 @@ type Props = {
   longitude: string;
   useRealTimeGeolocation?: boolean;
 };
+
+const polygons: PolygonData[] = [
+  {
+    coordinates: [
+      [10.08225749860928, -69.30652350819214],
+      [10.080841640221735, -69.30752680771464],
+      [10.078931867892194, -69.30879765377746],
+      [10.073136627430387, -69.30949996344309],
+      [10.072807349277454, -69.30097191749833],
+      [10.080413588786783, -69.29759414243797],
+      [10.081467252835154, -69.30137323730771],
+      [10.08225749860928, -69.30652350819214]
+    ],
+    title: "Polygon 1",
+    color: "blue",
+  },
+  {
+    coordinates: [
+      [10.068547892095253, -69.339039869577],
+      [10.065123334172199, -69.33877232303806],
+      [10.065650191602785, -69.33285285585278],
+      [10.069140600354501, -69.33322073234422],
+      [10.068547892095253, -69.339039869577]
+    ],
+    title: "Polygon 2",
+    color: "red",
+  },
+];
 
 function Map(props: Props) {
 
@@ -35,7 +70,10 @@ function Map(props: Props) {
     }
   }, [props.useRealTimeGeolocation, position]);
 
+  
+
   return (
+
     <div className="map">
       <MapContainer  center={position} zoom={13} scrollWheelZoom={true}>
         <TileLayer
@@ -44,9 +82,15 @@ function Map(props: Props) {
         />
         <Marker position={position}>
           <Popup>
-            Catedral de Barquisimeto. <br /> Lara
+            You're Here
           </Popup>
         </Marker>
+
+        {polygons.map((polygon, index) => (
+          <Polygon key={index} positions={polygon.coordinates} pathOptions={{ color: polygon.color }}>
+            <Tooltip>{polygon.title}</Tooltip>
+          </Polygon>
+        ))}
 
         <ChangeView center={position} zoom={13} />
       </MapContainer>
