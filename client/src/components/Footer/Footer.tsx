7f41362a14b37d4e037c00f06a98ae83f39f0b2d
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-05 20:52:41 -0400

    footer done!

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import "./Footer.scss";
+import { useEffect, useState } from "react";
 
 function Footer() {
+
+  const [currentYear, setCurrentYear] = useState("");
+
+  useEffect(() => {
+    const year = new Date().getFullYear();
+    setCurrentYear(year.toString()); 
+  }, []);
+
   return (
-    <div className='footer'>Footer</div>
-  )
+    <div className="footer">
+      <p className="pancratzia">
+        Made By{" "}
+        <Link className="link" to="https://github.com/Pancratzia">
+          Pancratzia
+        </Link>
+      </p>
+
+      <p className="copy">© {currentYear} - PancraLocation</p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
