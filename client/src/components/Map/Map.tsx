9f21bdeda887f0513802e6df40ecf3e0f2229520
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-05 22:35:48 -0400

    Map structure started

---
+++ client/src/components/Map/Map.tsx
@@ -0,0 +1,9 @@
+import "./Map.scss";
+
+function Map() {
+  return (
+    <div className="map">Map</div>
+  )
+}
+
+export default Map;
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,9 +1,38 @@
-import './Home.scss'
+import Map from "../../components/Map/Map";
+import "./Home.scss";
 
 function Home() {
   return (
-    <div className='home'>Home</div>
-  )
+    <div className="home">
+      <div className="container">
+        <h1>PancraLocation</h1>
+        <p>
+          Enter your coordinates or enable real-time geolocation to determine
+          your position on the map:
+        </p>
+
+        <hr />
+
+        <div className="locations">
+          <div className="location">
+            <label htmlFor="latitude">Latitude:</label>
+            <input type="text" id="latitude" />
+            <label htmlFor="longitude">Longitude:</label>
+            <input type="text" id="longitude" />
+            <button>Find</button>
+          </div>
+          <div className="realTime">
+            <button>Use Real Time Geolocation</button>
+          </div>
+
+          <div className="map">
+              <Map />
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
