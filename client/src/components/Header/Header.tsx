567e6f627105ebacbeff2882de465485d5712679
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-05 20:42:04 -0400

    header done!

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,40 @@
+import { Link } from "react-router-dom";
+import "./Header.scss";
+
 function Header() {
   return (
-    <div className="header">Header</div>
-  )
+    <div className="header">
+      <div className="site-name">
+        <h1>
+          <Link className="link" to="/">
+            Pancra<span>Location</span>
+          </Link>
+        </h1>
+        <p>
+          by{" "}
+          <Link className="link" to="https://github.com/Pancratzia">
+            Pancratzia
+          </Link>
+        </p>
+      </div>
+      <div className="container">
+        <div className="nav">
+          <ul className="links">
+            <li>
+              <Link className="link" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/config">
+                Configuration
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
