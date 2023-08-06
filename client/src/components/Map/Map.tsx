309e14fdf980d640b77d34c305564320d537a13b
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-05 22:56:00 -0400

    Adding leaflet map correctly. Styles not done yet

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,9 +1,27 @@
+import { MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
 import "./Map.scss";
+import { useState } from "react";
 
 function Map() {
+
+  const [position, setPosition] = useState({ lat: 10.073572859883452, lng: -69.32142913341524 });
+
   return (
-    <div className="map">Map</div>
-  )
+    <div className="map">
+      <MapContainer  center={position} zoom={15} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            Catedral de Barquisimeto. <br /> Lara
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Map;
