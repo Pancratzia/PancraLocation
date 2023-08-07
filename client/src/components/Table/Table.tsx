ddf6fee64cb1d600d46ebf152e69660dd03add9a
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-07 18:07:37 -0400

    Table Added

---
+++ client/src/components/Table/Table.tsx
@@ -0,0 +1,109 @@
+import Box from "@mui/material/Box";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import "./Table.scss";
+import { Link } from "react-router-dom";
+
+type Props = {
+  columns: GridColDef[];
+  rows: object[];
+};
+
+const Table = (props: Props) => {
+  const handleDelete = (id: number) => {
+    console.log("Your item has been deleted");
+  };
+
+  const actionColumn: GridColDef = {
+    field: "action",
+    headerName: "Action",
+    width: 150,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <Link to={`/config/${params.row.id}`}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-edit"
+              width="48"
+              height="48"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#000000"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
+              <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
+              <path d="M16 5l3 3" />
+            </svg>
+          </Link>
+
+          <div
+            className="delete"
+            onClick={() => {
+              handleDelete(params.row.id);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-trash"
+              width="48"
+              height="48"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#ff2825"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M4 7l16 0" />
+              <path d="M10 11l0 6" />
+              <path d="M14 11l0 6" />
+              <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+              <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+            </svg>
+          </div>
+        </div>
+      );
+    },
+  };
+
+  return (
+    <div className="table">
+      <Box sx={{ height: "auto", width: "100%" }}>
+        <DataGrid
+          className="dataGrid"
+          rows={props.rows}
+          columns={[...props.columns, actionColumn]}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          slots={{ toolbar: GridToolbar }}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
+              quickFilterProps: {
+                debounceMs: 500,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          disableRowSelectionOnClick
+          disableColumnFilter
+          disableDensitySelector
+          disableColumnSelector
+          
+        />
+      </Box>
+    </div>
+  );
+};
+
+export default Table;
--- client/src/pages/config/Config.tsx
+++ client/src/pages/config/Config.tsx
@@ -1,11 +1,39 @@
+
+import { GridColDef } from "@mui/x-data-grid";
+import Table from "../../components/Table/Table";
 import "./Config.scss";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID" },
+  {
+    field: "polygon",
+    headerName: "Polygon"
+    
+  },
+  {
+    field: "color",
+    headerName: "Color"
+  },
+];
+
+const rows = [
+  { id: 1, polygon: "One", color: "#111111"},
+  { id: 2, polygon: "Two", color: "#222222"},
+  { id: 3, polygon: "Three", color: "#333333"},
+  { id: 4, polygon: "Four", color: "#444444"},
+  { id: 5, polygon: "Five", color: "#555555"},
+];
 
 function Config() {
   return (
     <div className="config">
       <div className="container">
         
-        
+        <h1>Your Polygons</h1>
+
+        <button>Add New Polygon</button>
+
+        <Table columns={columns} rows={rows} />
 
       </div>
     </div>
