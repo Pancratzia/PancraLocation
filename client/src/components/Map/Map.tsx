324466289ef80b52f5c7655bec1e0f547da33613
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-08 22:02:27 -0400

    Project ready with default database with 3 polygons. Alerts to be done

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,8 +1,9 @@
 import { MapContainer, Marker, Polygon, Popup, TileLayer, Tooltip, useMap} from "react-leaflet";
-import 'leaflet/dist/leaflet.css';
-import "./Map.scss";
 import { useEffect, useState } from "react";
 import { LatLngExpression } from "leaflet";
+import axios from "axios";
+import 'leaflet/dist/leaflet.css';
+import "./Map.scss";
 
 interface Coordinates {
   lat: number;
@@ -10,9 +11,10 @@ interface Coordinates {
 }
 
 interface PolygonData {
-  coordinates: LatLngExpression[];
-  title: string;
+  id: number;
+  name: string;
   color: string;
+  coordinates: { latitude: number; longitude: number }[];
 }
 
 type Props = {
@@ -21,40 +23,13 @@ type Props = {
   useRealTimeGeolocation?: boolean;
 };
 
-const polygons: PolygonData[] = [
-  {
-    coordinates: [
-      [10.08225749860928, -69.30652350819214],
-      [10.080841640221735, -69.30752680771464],
-      [10.078931867892194, -69.30879765377746],
-      [10.073136627430387, -69.30949996344309],
-      [10.072807349277454, -69.30097191749833],
-      [10.080413588786783, -69.29759414243797],
-      [10.081467252835154, -69.30137323730771],
-      [10.08225749860928, -69.30652350819214]
-    ],
-    title: "Polygon 1",
-    color: "#555",
-  },
-  {
-    coordinates: [
-      [10.068547892095253, -69.339039869577],
-      [10.065123334172199, -69.33877232303806],
-      [10.065650191602785, -69.33285285585278],
-      [10.069140600354501, -69.33322073234422],
-      [10.068547892095253, -69.339039869577]
-    ],
-    title: "Polygon 2",
-    color: "#333",
-  },
-];
-
 function Map(props: Props) {
 
   const [position, setPosition] = useState<Coordinates>({
     lat: parseFloat(props.latitude) || 0,
     lng: parseFloat(props.longitude) || 0,
   });
+  const [polygons, setPolygons] = useState<PolygonData[]>([]);
 
   useEffect(() => {
     setPosition({
@@ -70,7 +45,15 @@ function Map(props: Props) {
     }
   }, [props.useRealTimeGeolocation, position]);
 
-  
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/polygons')
+      .then(response => {
+        setPolygons(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching polygons:", error);
+      });
+  }, []);
 
   return (
 
@@ -87,8 +70,12 @@ function Map(props: Props) {
         </Marker>
 
         {polygons.map((polygon, index) => (
-          <Polygon key={index} positions={polygon.coordinates} pathOptions={{ color: polygon.color }}>
-            <Tooltip>{polygon.title}</Tooltip>
+          <Polygon
+            key={polygon.id}
+            positions={polygon.coordinates.map(coord => [coord.latitude, coord.longitude])}
+            pathOptions={{ color: polygon.color }}
+          >
+            <Tooltip>{polygon.name}</Tooltip>
           </Polygon>
         ))}
 
