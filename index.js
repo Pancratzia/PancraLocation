e2edd0eaeb19fbcee61e078aaa1a305e6ab334ae
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-10 11:59:16 -0400

    Production V1

--- index.js
+++ index.js
@@ -167,10 +167,10 @@ app.delete('/api/polygons/:id', (req, res) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
